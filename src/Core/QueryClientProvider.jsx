0a29cdc8aa966066ff4fa145b80747d47977c7e1
AbdelrhmanAmin <abdo.amin9991@gmail.com>
2022-09-11 19:02:37 +0200

    fetch on focus

--- src/Core/QueryClientProvider.jsx
+++ src/Core/QueryClientProvider.jsx
@@ -3,6 +3,23 @@ import React from "react";
 const context = React.createContext();
 
 const QueryClientProvider = ({ children, client }) => {
+  React.useEffect(() => {
+    const onFocus = () => {
+      client.queries.forEach((query) => {
+        query.subscribers.forEach((subscriber) => {
+          subscriber.fetch();
+        });
+      });
+    };
+
+    window.addEventListener("visibilitychange", onFocus, false);
+    window.addEventListener("focus", onFocus, false);
+
+    return () => {
+      window.removeEventListener("visibilitychange", onFocus);
+      window.removeEventListener("focus", onFocus);
+    };
+  }, [client]);
   return <context.Provider value={client}>{children}</context.Provider>;
 };
 
