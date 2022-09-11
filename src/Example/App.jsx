947b8e0f5ab1f5f710f9e2ab7377fbcf588749cb
AbdelrhmanAmin <abdo.amin9991@gmail.com>
2022-09-11 18:25:17 +0200

    example

--- src/Example/App.jsx
+++ src/Example/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import { useQuery } from "../Core";
+
+const fetchTodos = (count = 1) =>
+  fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${count}`)
+    .then((res) => res.json())
+    .then((data) => data);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  const { data: todos = [] } = useQuery("todos", () => fetchTodos(10));
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
